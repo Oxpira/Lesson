d0d94b7306d12759758e1f188df2f9bab1d0abac
Oxpira <69744003+Oxpira@users.noreply.github.com>
2020-08-17 13:38:43 +0700

    Add files via upload

---
+++ Lesson/src/ru/sbrf/leaason/Main.java
@@ -0,0 +1,10 @@
+package ru.sbrf.leaason;
+// todo: dsaf
+public class Main {
+    public static void main(String[] args) {
+        String bob = "Хелло Боб";
+        System.out.println(bob);
+//        System.exit(6);
+    }
+
+}
