70e9e39b32fe3ad8bd0e581bf7a0e9ff6aae2113
Oxpira <69744003+Oxpira@users.noreply.github.com>
2020-08-17 18:33:42 +0700

    Add files via upload

--- Lesson/src/ru/sbrf/leaason/Main.java
+++ Lesson/src/ru/sbrf/leaason/Main.java
@@ -12,12 +12,18 @@ public class Main {
         int b3 = 99;
         char i = 'X';
         char i1 = '6';
+        char j = 'A';
 
         System.out.println("Задание 4: "+sum(a1, b1));
         System.out.println("Задание 5: "+proizv(a2, b2));
         System.out.println("Задание 6: "+getMaxNumber(a3, b3));
         System.out.println("Задание 7: "+isCharA(i));
         System.out.println("Задание 8: "+isCharNumber(i1));
+        bynar();
+        MAX();
+        System.out.println("Задание 11: "+CharInt(j));
+        System.out.println("Задание 12: "+IntChar(a2));
+
     }
 
     public static byte sum(byte a1, short b1) {
@@ -39,5 +45,22 @@ public class Main {
     public static boolean isCharNumber(char i1) {
         return Character.isDigit(i1);
 
+    }
+    public static void bynar() {
+        for (int i2=0; i2<31; i2++) {
+            System.out.println("Задание 9: " + Integer.toBinaryString(i2));
+        }
+    }
+    public static void MAX() {
+
+        System.out.println("Задание 10: " +Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println("Задание 10: " +Integer.toBinaryString(Integer.MAX_VALUE+1));
+    }
+
+    public static int CharInt(char i) {
+        return (int) i;
+    }
+    public static char IntChar(int i) {
+        return (char) i;
     }
 }
